af82bba426b8c1bfd4083100b420c5439722304b
SolMarina <sparraa17@gmail.com>
2020-12-01 22:30:33 -0300

    con slide

--- src/Views/View2.js
+++ src/Views/View2.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import styles from './View2.module.css'
 import lol from "../img/cartadelcampeon.png"
 import burguer from "../img/tablet1.png"
@@ -6,159 +6,55 @@ import barky from "../img/mobile.png"
 import comida from '../img/logocolorS.png'
 import mdlink from '../img/mdnode.png'
 import credit from '../img/validacion.png'
+import M from "materialize-css";
+import React, {useEffect } from 'react';
+
+
+
+
+
 const View2 = () => {
+
+useEffect(() => {
+    const slider = document.querySelectorAll('.slider');
+    const instances = M.Slider.init(slider, {
+        indicators: true,
+        duration: 400
+
+    });
+})
     return (
         <div>
-            <h3>Mis Proyectos </h3>
+            
+            <div className="slider">
+                <ul className="slides">
 
-            <div className={styles.boxprincipal1}>
-                <div className={styles.box1}>
-                    <div class="card small">
-
-                        <div class="row">
-                            <div class="col s12 m7">
-                                <div class="card #0097a7 cyan darken-2" >
-                                    <div class="card-image">
-                                        <img className={styles.lol} src={lol} alt="">
-                                        </img>
-                                        <span class="card-title"></span>
-
-                                        <div class="card-content">
-                                            <p class="cyan-text text-darken-3"> DataLovers: Javascript /html5/ API / CSS3/ Figma / Github/ Hot Jar / Balsamiq Mockups</p>
-                                        </div>
-                                        <div class="card-action">
-                                            <a href="https://algrismarquez.github.io/SCL014-data-lovers/index.html">Data Lovers</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <li>
+                        <img src="https://lorempixel.com/580/250/nature/1"></img>
+                        <div class="caption center-align">
+                            <h1> holaaaaaaa</h1>
+                            <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
                         </div>
-
-                    </div>
-                </div>
-                <div className={styles.box2}>
-                    <div class="card small">
-                        <div class="row">
-                            <div class="col s12 m7">
-                                <div class="card #0097a7 cyan darken-2">
-                                    <div class="card-image">
-                                        <img className={styles.burguer} src={burguer} alt=""></img>
-                                        <span class="card-title"></span>
-                                        <div class="card-content">
-                                            <p>Burguer Queen: Single-page Application (SPA) React / Firebase / CSS3 / Figma / Github.</p>
-                                        </div>
-                                        <div class="card-action">
-                                            <a href="https://burgersong.on.fleek.co/">Burguer Queen</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
+                    </li>
+                    <li>
+                        <img src="https://lorempixel.com/580/250/nature/1"></img>
+                        <div class="caption center-align">
+                            <h3>This is our big Tagline!</h3>
+                            <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
                         </div>
-                    </div>
-                </div>
-
-                <div className={styles.box3}>
-                    <div class="card small">
-                        <div class="row">
-                            <div class="col s12 m7">
-                                <div class="card #0097a7 cyan darken-2">
-                                    <div class="card-image">
-                                        <img className={styles.barky} src={barky} alt=""></img>
-                                        <span class="card-title"></span>
-
-                                        <div class="card-content">
-                                            <p>Red Social: Single-page Application (SPA) Javascript /Firebase /CSS3/ Figma /Github.</p>
-                                        </div>
-                                        <div class="card-action">
-                                            <a href="#">Red Social</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
+                    </li>
+                    <li>
+                        <img src="https://lorempixel.com/580/250/nature/1"></img>
+                        <div class="caption center-align">
+                            <h3>This is our big Tagline!</h3>
+                            <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
                         </div>
-                    </div>
-                </div>
+                    </li>
 
-
+                </ul>
             </div>
-
-            <div className={styles.boxprincipal1}>
-                <div className={styles.box1}>
-                    <div class="card small">
-                        <div class="row">
-                            <div class="col s12 m7">
-                                <div class="card #0097a7 cyan darken-2" >
-                                    <div class="card-image">
-                                        <img className={styles.comi} src={comida} alt="">
-                                        </img>
-                                        <span class="card-title"></span>
-
-                                        <div class="card-content">
-                                            <p class="cyan-text text-darken-3"> Comida para todos:  Javascript / React / CSS3/ Figma / Github/Boostrap</p>
-                                        </div>
-                                        <div class="card-action">
-                                            <a href="https://comidaparatodos.on.fleek.co/">Comida Para Todos</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className={styles.box2}>
-                    <div class="card small">
-                        <div class="row">
-                            <div class="col s12 m7">
-                                <div class="card #0097a7 cyan darken-2">
-                                    <div class="card-image">
-                                        <img className={styles.mdlink} src={mdlink} alt=""></img>
-                                        <span class="card-title"></span>
-                                        <div class="card-content">
-                                            <p>MdLinks:  Libreria realizada con Node.js/ Markdow-it/ Fetch /JSDom</p>
-                                        </div>
-                                        <div class="card-action">
-                                            <a href="https://www.npmjs.com/package/md-links-solex">md-links-solex</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className={styles.box3}>
-                    <div class="card small">
-                        <div class="row">
-                            <div class="col s12 m7">
-                                <div class="card #0097a7 cyan darken-2">
-                                    <div class="card-image">
-                                        <img className={styles.credit} src={credit} alt=""></img>
-                                        <span class="card-title"></span>
-
-                                        <div class="card-content">
-                                            <p>Validador de Tarjeta: Javascript /CSS3 /html5 /Github /Balsamiq Mockups</p>
-                                        </div>
-                                        <div class="card-action">
-                                            <a href="https://solmarina.github.io/SCL014-card-validation/">Card Validator</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-
 
         </div>
-
 
     );
 };
