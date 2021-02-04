a94d7254874bc547393c884ad74677fb09b97268
SolMarina <sparraa17@gmail.com>
2021-02-04 18:32:26 -0300

    cambios para mostrar proyectos

--- src/Views/View2.js
+++ src/Views/View2.js
@@ -2,94 +2,141 @@ import burguer from "../img/tablet1.png"
 import barky from "../img/mobile.png"
 import comida from '../img/logocolorS.png'
 import mdlink from '../img/mdnode.png'
-import M from "materialize-css";
-import React, { useEffect } from 'react';
+import lovers from '../img/cartadelcampeon.png'
+import mach from '../img/mach.png'
+import styles from "./View2.module.css"
 
 const View2 = () => {
-
-    useEffect(() => {
-        const slider = document.querySelectorAll('.slider');
-        const instances = M.Slider.init(slider, {
-            indicators: true,
-            duration: 400,
-            height: 600
-
-        });
-    })
     return (
         <div id="proyectos">
-
-            <div className="slider" >
-                <ul className="slides">
-
-                    <li>
-                        <img class="responsive-img" src={mdlink}></img>
-                        <div class="row">
-                            <div class="col s12 m6">
-                                <div class="card black caption center-align">
-                                    <div class="card-content cyan-text text-darken-3">
-                                        <h4 class="card-title">MdLinks</h4>
-                                        <p class="cyan-text text-darken-3">Librería realizada con Node.js enfocada en leer y analizar archivos en formato Markdown, para verificar los links que contengan y reportar algunas estadísticas.</p>
-                                    </div>
-                                    <div class="card-action">
-                                        <a href="https://www.npmjs.com/package/md-links-solex">MdLinks</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <img class="responsive-img" src={comida}></img>
-                        <div class="row">
-                            <div class="col s12 m6">
-                                <div class="card amber lighten-5 caption center-align">
-                                    <div class="card-content cyan-text text-darken-4">
-                                        <h4 class="card-title">Comida Para todos</h4>
-                                        <p class="cyan-text text-darken-4">Desarrollado con React.js, el proyecto tuvo como meta principal mejorar el look and feel de la página de la Fundación Comida para Todos</p>
-                                    </div>
-                                    <div class="card-action">
-                                        <a href="https://comidaparatodos.on.fleek.co/">Comida Para Todos</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <img class="responsive-img" src={burguer}></img>
-                        <div class="row">
-                            <div class="col s12 m6">
-                                <div class="card amber lighten-5 caption center-align">
-                                    <div class="card-content cyan-text text-darken-4">
-                                        <h4 class="card-title">Burguer Queen</h4>
-                                        <p class="cyan-text text-darken-4">Aplicación desarrollada y diseñada para tablets , es un sistema que ayuda a tomar los pedidos de clientes de un restaurante.SPA,React,Firebase,CSS3</p>
-                                    </div>
-                                    <div class="card-action">
-                                        <a href="https://burgersong.on.fleek.co/">Burguer Song</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <img class="responsive-img" src={barky}></img>
-                        <div class="row">
-                            <div class="col s12 m6">
-                                <div class="card amber lighten-5 caption center-align">
-                                    <div class="card-content cyan-text text-darken-4">
-                                        <h4 class="card-title">Red Social</h4>
-                                        <p class="cyan-text text-darken-4">Aplicación creada con el propósito de generar interacción y sentido de manada entre perros que se encuentren geográficamente cerca y presenten ciertas afinidades.SPA,Javascript,Firebase,CSS3</p>
-                                    </div>
-                                    <div class="card-action">
-                                        <a href="https://mcmacarena.github.io/SCL014-social-network/">Barkify</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-
-                </ul>
+            <div>
+                <h1>Proyectos</h1>
             </div>
 
+            <div className={styles.box1}>
+                <div className={styles.box11}>
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img className="activator" src={burguer} alt=""></img></div>
+
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Burger Queen<i class="material-icons right">more_vert</i></span>
+                            <div className={styles.links}>
+                                <p><a href="https://burgersong.on.fleek.co/">Demo</a></p>
+                                <p><a href="https://github.com/SolMarina/burger-queen">Github</a></p>
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Burger Queen<i class="material-icons right">close</i></span>
+                            <p>Single-page Application (SPA) React.js/React hooks/ Firebase / CSS3 / Figma / Github. Es una app desarrollada y diseñada para tablets , un sistema que ayude a tomar los pedidos de clientes de un restaurante.
+</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.box11}>
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img className="activator" src={barky} alt=""></img></div>
+
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Barkyfi<i class="material-icons right">more_vert</i></span>
+                            <div className={styles.links}>
+                                <p><a href="https://mcmacarena.github.io/SCL014-social-network/">Demo</a></p>
+                                <p><a href="https://github.com/SolMarina/SCL014-social-network">Github</a></p>
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Barkify<i class="material-icons right">close</i></span>
+                            <p>Single-page Application (SPA) Javascript / Firebase / CSS3/ Figma / Github. Red Social creada con el propósito de generar interacción y sentido de manada entre perros que se encuentren geográficamente cerca y presenten ciertas afinidades.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.box1}>
+                <div className={styles.box11}>
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img className="activator" src={comida} alt=""></img></div>
+
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Comida Para Todos<i class="material-icons right">more_vert</i></span>
+                            <div className={styles.links}>
+                                <p><a href="https://comidaparatodos.on.fleek.co/">Demo</a></p>
+                                <p><a href="https://github.com/SolMarina/ComidaParaTodos">Github</a></p>
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Comida para todos<i class="material-icons right">close</i></span>
+                            <p>Desarrollado con React.js, CSS3. El proyecto tiene meta principal solucionar el dolor de la Fundación Comida para Todos, enfocandose en mostrar a los futuros donantes de forma transparente, el destino de las donaciones, maneras de hacer donaciones, historias de personas que recebieron ayuda.
+
+Con la colaboración entre estudiantes de Ux y Front-End, mejoramos el design de la página web, dejandola más interesante y así obtenendo una mejor experiencia para el usuario.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.box11}>
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img className="activator" src={mdlink} alt=""></img></div>
+
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">MdLinks<i class="material-icons right">more_vert</i></span>
+                            <div className={styles.links}>
+                                <p><a href="https://www.npmjs.com/package/md-links-solex">Demo</a></p>
+                                <p><a href="https://github.com/SolMarina/SCL014-md-links">Github</a></p>
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">MdLinks<i class="material-icons right">close</i></span>
+                            <p>Librería realizada con Node.js enfocada en leer y analizar archivos en formato Markdown, para verificar los links que contengan y reportar algunas estadísticas.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className={styles.box1}>
+                <div className={styles.box11}>
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img className="activator" src={lovers} alt=""></img></div>
+
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Data lovers<i class="material-icons right">more_vert</i></span>
+                            <div className={styles.links}>
+                                <p><a href="https://solmarina.github.io/SCL014-data-lovers/">Demo</a></p>
+                                <p><a href="https://github.com/SolMarina/SCL014-data-lovers">Github</a></p>
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Data Lovers<i class="material-icons right">close</i></span>
+                            <p>Javascript /CSS3/ Figma. Página web para visualizar un conjunto (set) de datos que se adecúa a las necesidades de los jugadores de League of Legend .
+</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.box11}>
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img className="activator" src={mach} alt=""></img></div>
+
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">MACH<i class="material-icons right">more_vert</i></span>
+                            <div className={styles.links}>
+                                <p><a href="https://mach-app-9892d.firebaseapp.com/">Demo</a></p>
+                                <p><a href="https://github.com/SolMarina/SCL014-Hackathon">Github</a></p>
+                            </div>
+
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Sección de Beneficios<i class="material-icons right">close</i></span>
+                            <p>Sección de Beneficios, realizada en 4 días de Hackathon para MACH, es una aplicaciòn realizada para mobile, utilizando react.js y
+Bootstrap </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
